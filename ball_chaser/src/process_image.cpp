8251ed9262c2e38f8a7f0fde68148da76c9cf92d
salabson <salabson4@yahoo.co.uk>
2019-05-10 15:51:47 +0100

    Update position of the robot in the world

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -19,12 +19,31 @@ void drive_robot(float lin_x, float ang_z)
     }
 }
 
+int count_color_pixel(int start, int end,int pixel_color, const sensor_msgs::Image img)
+{
+    int num_of_white_pixel=0;
+    
+       for(int y=start; y<=img.height*end; y++)
+       {
+          if (img.data[y] == pixel_color)
+          {
+            num_of_white_pixel++;
+          } 
+       }
+ 
+   return num_of_white_pixel;
+
+}
+ 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
     bool ballPresent = false;
+    int num_of_white_pixel_left =0;
+    int num_of_white_pixel_center =0;
+    int num_of_white_pixel_right =0;
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
@@ -39,8 +58,13 @@ void process_image_callback(const sensor_msgs::Image img)
     
    if(ballPresent)
    {
+    int side = img.step/3;
+     num_of_white_pixel_left =count_color_pixel(0,side,white_pixel,img);
+     num_of_white_pixel_center =count_color_pixel(side,side*2,white_pixel,img); 
+     num_of_white_pixel_right=count_color_pixel(side*2,side*3,white_pixel,img);
+     ROS_INFO("left: %d center: %d right: %d ", num_of_white_pixel_left, num_of_white_pixel_center, num_of_white_pixel_right);
      drive_robot(0.5,0.0);
-   }
+   } else {drive_robot(0.0, 0.0 );}
 }
 
 
