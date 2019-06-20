bf5670b0c99827dfc775728780d91da6eb38b13c
salabson <salabson4@yahoo.co.uk>
2019-06-20 11:16:35 +0100

    Create shell scripts for slam

---
+++ my_robot/scripts/tesh.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/salab_world" &
+
+sleep 5
+
+xterm -e "roslaunch  turtlebot_gazebo gmapping_demo.launch" &
+
+sleep 5
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+sleep 5
+
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
+
+
