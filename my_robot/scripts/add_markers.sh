ef58bb9d7741748757788efd2815094957a5f8a0
salabson <salabson4@yahoo.co.uk>
2019-07-03 12:33:23 +0100

    Create shell script file that launch robot and the marker

--- my_robot/scripts/add_markers.sh
+++ my_robot/scripts/add_markers.sh
@@ -12,7 +12,7 @@ xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 
 sleep 5
 
-xterm -e "roslaunch pick_objects  pick_objects.launch"
+xterm -e "rosrun  add_marker  add_marker"
 
 
 
